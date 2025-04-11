4a06fd4f4be9cfb726af9f22a1024297cd6d09bb
Karam Al-Haidari <fs58fryfbyqc@gmail.com>
2025-04-11 17:30:59 +0300

    dono

--- Form1.vb
+++ Form1.vb
@@ -1,15 +1,15 @@
 ﻿Public Class Form1
     Private Sub frmIntro_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' Form properties
+
         Me.Text = "Iraqi Airways"
         Me.Width = 600
         Me.Height = 450
-        Me.StartPosition = FormStartPosition.CenterScreen
+
         Me.FormBorderStyle = FormBorderStyle.FixedSingle
-        Me.MaximizeBox = False
 
 
-        ' Logo/Titles
+
+
         Dim lblTitle As New Label()
         lblTitle.Text = "WELCOME TO IRAQI AIRWAYS"
 
@@ -20,7 +20,6 @@
         lblTitle.Height = 40
         Me.Controls.Add(lblTitle)
 
-        ' Subtitle
         Dim lblSubtitle As New Label()
         lblSubtitle.Text = "Your simple flight booking solution"
         lblSubtitle.Font = New Font("Arial", 12, FontStyle.Italic)
@@ -30,7 +29,7 @@
         lblSubtitle.Height = 30
         Me.Controls.Add(lblSubtitle)
 
-        ' Services Group
+
         Dim grpServices As New GroupBox()
         grpServices.Text = "Our Services"
         grpServices.Location = New Point(50, 120)
@@ -38,7 +37,7 @@
         grpServices.Height = 200
         Me.Controls.Add(grpServices)
 
-        ' Booking Button
+
         Dim btnBooking As New Button()
         btnBooking.Text = "Flight Booking"
         btnBooking.Location = New Point(30, 40)
@@ -49,17 +48,15 @@
         grpServices.Controls.Add(btnBooking)
         AddHandler btnBooking.Click, AddressOf btnBooking_Click
 
-        ' Check Status Button
         Dim btnStatus As New Button()
-        btnStatus.Text = "Check Flight Status"
+        btnStatus.Text = "Available Flights"
         btnStatus.Location = New Point(260, 40)
         btnStatus.Width = 200
         btnStatus.Height = 50
         btnStatus.Font = New Font("Arial", 12)
         grpServices.Controls.Add(btnStatus)
-        AddHandler btnStatus.Click, AddressOf btnStatus_Click
+        AddHandler btnStatus.Click, AddressOf btnAvaliableFlights_Click
 
-        ' Check-in Button
         Dim btnCheckin As New Button()
         btnCheckin.Text = "Online Check-in"
         btnCheckin.Location = New Point(30, 120)
@@ -69,7 +66,6 @@
         grpServices.Controls.Add(btnCheckin)
         AddHandler btnCheckin.Click, AddressOf btnCheckin_Click
 
-        ' Customer Support Button
         Dim btnSupport As New Button()
         btnSupport.Text = "Customer Support"
         btnSupport.Location = New Point(260, 120)
@@ -79,7 +75,6 @@
         grpServices.Controls.Add(btnSupport)
         AddHandler btnSupport.Click, AddressOf btnSupport_Click
 
-        ' Exit Button
         Dim btnExit As New Button()
         btnExit.Text = "Exit"
         btnExit.Location = New Point(250, 350)
@@ -89,30 +84,27 @@
         AddHandler btnExit.Click, AddressOf btnExit_Click
     End Sub
 
-    ' Booking Button Click Event
     Private Sub btnBooking_Click(sender As Object, e As EventArgs)
-        Dim bookingForm As New Form2()
-        bookingForm.ShowDialog()
+
+        Form2.ShowDialog()
 
     End Sub
 
-    ' Check Status Button Click Event
-    Private Sub btnStatus_Click(sender As Object, e As EventArgs)
-        MessageBox.Show("Flight Status feature is coming soon.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+    Private Sub btnAvaliableFlights_Click(sender As Object, e As EventArgs)
+        Form3.Show()
     End Sub
 
-    ' Check-in Button Click Event
     Private Sub btnCheckin_Click(sender As Object, e As EventArgs)
         MessageBox.Show("Online Check-in feature is coming soon.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
     End Sub
 
-    ' Customer Support Button Click Event
     Private Sub btnSupport_Click(sender As Object, e As EventArgs)
         MessageBox.Show("Customer Support: +964-772-883-3423" & vbCrLf & "Email: support@iraqairways.gov", "Customer Support", MessageBoxButtons.OK, MessageBoxIcon.Information)
     End Sub
 
-    ' Exit Button Click Event
+
     Private Sub btnExit_Click(sender As Object, e As EventArgs)
-        Application.Exit()
+        Me.Close()
     End Sub
 End Class
---
+++ Form3.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form3
+
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.Text = "Iraqi Airways Flight History"
+        Me.Size = New Size(1300, 550)
+
+
+        Dim dataGridView = New DataGridView()
+        dataGridView.Name = "dataGridView"
+        dataGridView.Location = New Point(20, 60)
+        dataGridView.Size = New Size(1200, 400)
+        dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+        dataGridView.Columns.Add("FlightNumber", "Flight Number")
+        dataGridView.Columns.Add("DepartureTime", "Departure Time")
+        dataGridView.Columns.Add("ArrivalTime", "Arrival Time")
+        dataGridView.Columns.Add("Status", "Status")
+        dataGridView.Columns.Add("AircraftType", "Aircraft Type")
+        dataGridView.Columns.Add("DepartureAirport", "Departure Airport")
+        dataGridView.Columns.Add("ArrivalAirport", "Arrival Airport")
+        dataGridView.Columns.Add("FlightDuration", "Flight Duration")
+        Me.Controls.Add(dataGridView)
+
+
+        dataGridView.Rows.Add("IA123", "2025-04-10 08:00", "2025-04-10 11:00", "On Time", "Airbus A320", "Baghdad International", "Dubai International", "3h 0m")
+        dataGridView.Rows.Add("IA124", "2025-04-10 14:00", "2025-04-10 17:00", "Delayed", "Boeing 737", "Baghdad International", "Cairo International", "3h 0m")
+        dataGridView.Rows.Add("IA125", "2025-04-10 18:00", "2025-04-10 21:00", "On Time", "Airbus A330", "Basra International", "Amman International", "3h 0m")
+        dataGridView.Rows.Add("IA126", "2025-04-10 22:00", "2025-04-11 01:00", "Delayed", "Boeing 777", "Baghdad International", "Beirut International", "4h 0m")
+        dataGridView.Rows.Add("IA127", "2025-04-11 06:00", "2025-04-11 09:00", "Cancelled", "Airbus A310", "Najaf International", "Tehran Imam Khomeini", "3h 0m")
+        dataGridView.Rows.Add("IA128", "2025-04-11 11:00", "2025-04-11 14:00", "On Time", "Boeing 787", "Baghdad International", "Frankfurt Airport", "5h 0m")
+        dataGridView.Rows.Add("IA129", "2025-04-11 16:00", "2025-04-11 19:00", "On Time", "Airbus A350", "Sulaymaniyah Airport", "Istanbul Airport", "3h 0m")
+    End Sub
+
+
+End Class
+
